c28791a085be999502ae8f833f6ae703c9d621bc
AlexeyAZharkov <azharkov@mail.ru>
2021-11-22 13:19:33 +0300

    Initial commit

---
+++ forms.py
@@ -0,0 +1,34 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditorField
+
+
+##WTForm
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
+
+
+##WTForm
+class CreateUserForm(FlaskForm):
+    email = StringField("Enter email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    name = StringField("Enter Your Name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+##WTForm
+class LoginUserForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+##WTForm
+class CreateCommentForm(FlaskForm):
+    text = CKEditorField("Blog Comment", validators=[DataRequired()])
+    submit = SubmitField("Post comment")
